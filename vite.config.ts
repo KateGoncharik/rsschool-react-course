7c08b9f0baf37cc34e582ff8166e6b7b32993b8d
rakr0819 <raman.krauchanka@netcracker.com>
2023-10-30 00:30:00 +0300

    fix: test lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+const a;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
