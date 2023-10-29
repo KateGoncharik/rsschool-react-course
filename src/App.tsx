ec01843338fd0175b512fa03a6f3fc03396fdbb4
rakr0819 <raman.krauchanka@netcracker.com>
2023-10-29 12:14:08 +0300

    init: start add

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+function App() {
+    return (
+        <div>
+            <div>Hi!</div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+function WrappedApp() {
+    return (
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    )
+}
+
+export default WrappedApp
