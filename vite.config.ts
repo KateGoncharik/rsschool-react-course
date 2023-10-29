b328d083aa3260bdb7dac1ba1c1409dd4fdec0bd
rakr0819 <raman.krauchanka@netcracker.com>
2023-10-30 00:32:47 +0300

    Checking husky

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 const a;
+const b;
+console.log("foo");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
