a67a54c93a5db828e3670ab9ac81725d2343e576
rakr0819 <raman.krauchanka@netcracker.com>
2023-10-29 12:40:09 +0300

    init: start react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
-    return (
-        <div>
-            <div>Hi!</div>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </div>
-    )
+  return (
+    <div>
+      <div>Hi!</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 function WrappedApp() {
-    return (
-        <HashRouter>
-            <App/>
-        </HashRouter>
-    )
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
 }
 
-export default WrappedApp
+export default WrappedApp;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-    return (
-        <>
-            <h1>Not Found</h1>
-            <Link to="/">Go Home</Link>
-        </>
-    )
+  return (
+    <>
+      <h1>Not Found</h1>
+      <Link to="/">Go Home</Link>
+    </>
+  );
 }
